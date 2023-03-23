58ba06561ce50bf5dcfb287969cbe5d214dd7351
Sergio Reinoso <slrs993@gmail.com>
2023-03-22 21:20:32 -0500

    Agregando el proyecto dominando collections

---
+++ src/com/alura/model/Curso.java
@@ -0,0 +1,56 @@
+package com.alura.model;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Curso {
+
+    private String nombre;
+    private int tiempo;
+    private List<Aula> aulaList = new LinkedList<>();
+
+    public Curso(String nombre, int tiempo) {
+        this.nombre = nombre;
+        this.tiempo = tiempo;
+    }
+
+    public Curso(String nombre, int tiempo, List<Aula> aulaList) {
+        this.nombre = nombre;
+        this.tiempo = tiempo;
+        this.aulaList = aulaList;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public List<Aula> getAulaList() {
+        return Collections.unmodifiableList(aulaList);
+    }
+
+    public void setAulaList(List<Aula> aulaList) {
+        this.aulaList = aulaList;
+    }
+
+    public void addAula(Aula aula){
+        this.aulaList.add(aula);
+    }
+
+    @Override
+    public String toString(){
+        return this.nombre;
+    }
+}
