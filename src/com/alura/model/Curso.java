c9888ddee4f79b7588f43c9c5b607b9c54331e63
Sergio Reinoso <slrs993@gmail.com>
2023-03-30 23:22:24 -0500

    Finalizacion del curso

--- src/com/alura/model/Curso.java
+++ src/com/alura/model/Curso.java
@@ -8,6 +8,9 @@ public class Curso {
     private int tiempo;
     private List<Aula> aulaList = new ArrayList<>();
     private Collection<Alumno> alumnos = new HashSet<>();
+    //private Collection<Alumno> alumnos = new LinkedHashSet<>();
+    private Map<String, Alumno> alumnoMap = new HashMap<>();
+
 
     public Curso(String nombre, int tiempo) {
         this.nombre = nombre;
@@ -48,12 +51,16 @@ public class Curso {
         this.aulaList.add(aula);
     }
 
-    public void addAlumno(Alumno alumno) {this.alumnos.add(alumno);}
+    public void addAlumno(Alumno alumno) {this.alumnos.add(alumno); this.alumnoMap.put(alumno.getCodigo(), alumno);}
 
     public boolean verificaAlumno(Alumno alumno) {return this.alumnos.contains(alumno);}
 
     public Collection<Alumno> getAlumnos(){
         return alumnos;
+    }
+
+    public Map<String, Alumno> getAlumnoMap() {
+        return alumnoMap;
     }
 
     @Override
